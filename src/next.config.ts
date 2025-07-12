6375f9ce2de773301b2a3acb2a007ca3ed71f19c
MNU Diego Abner <DiegoManiNhU@gmail.com>
2025-07-12 22:00:38 +0000

    ok, vamos para o passo Set up backend. Não achei a opção, apos Reconstru

---
+++ src/next.config.ts
@@ -0,0 +1,37 @@
+
+import type {NextConfig} from 'next';
+
+// Forcing a change to trigger environment reload
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com', // Added for Firebase Storage
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'api.qrserver.com', // Added for QR Code generation
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
